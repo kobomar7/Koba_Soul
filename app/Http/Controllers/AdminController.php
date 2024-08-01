1e536eab347bc3fc4abc68e60b5de877b82ecb62
kobomar7 <kobomar8@gmail.com>
2024-08-01 15:58:32 +0000

    First commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+
+class AdminController extends Controller
+{
+    public function view_category()
+    {
+        $data = Category::all();
+        return view('admin.category',compact('data'));
+
+    }
+    public function add_category(Request $request)
+    {
+        $category = new Category;
+        $category->category_name = $request->category;
+        $category->save();
+        toastr()->timeOut(1000)->closeButton()->addSuccess('Category Added Successfully.');
+        return redirect()->back();
+
+    }
+    public function delete_category($id)
+    {
+        $data = Category::findOrFail($id);
+        $data->delete();
+        toastr()->timeOut(1000)->closeButton()->addSuccess('Category Deleted Successfully.');
+
+        return redirect()->back();
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
+
+use App\Http\Middleware\Admin;
+
+route::get('/',[HomeController::class,'home']);
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+route::get('dashboard',[HomeController::class,'index'])->middleware(['auth','admin']);
+
+route::get('view_category',[AdminController::class,'view_category'])->middleware(['auth','admin']);
+
+route::post('add_category',[AdminController::class,'add_category'])->middleware(['auth','admin']);
+
+route::delete('delete_category/{id}',[AdminController::class,'delete_category'])->middleware(['auth','admin']);
